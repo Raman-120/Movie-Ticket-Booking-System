b7b3e51d88e1471841d6cbe1129c1848edc7cae0
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-28 21:40:38 +0545

    set up Movie and Show pages

--- Movie Ticket Booking System/src/model/Movie.java
+++ Movie Ticket Booking System/src/model/Movie.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Movie {
+
+    private String movieName;
+    private String movieGenre;
+    private int duration;
+
+    Movie(String movieName, String movieGenre, int duration){
+        this.movieName = movieName;
+        this.movieGenre = movieGenre;
+        this.duration = duration;
+    }
+
+    public String getMovieName(){
+        return this.movieName;
+    }
+
+    public String getMovieGenre(){
+        return this.movieGenre;
+    }
+
+    public int getDuration(){
+        return this.duration;
+    }
+
 }
--- Movie Ticket Booking System/src/model/Show.java
+++ Movie Ticket Booking System/src/model/Show.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Show {
+
+    private String showName;
+    private String showGenre;
+    private int duration;
+
+    Show(String movieName, String movieGenre, int duration){
+        this.showName = movieName;
+        this.showGenre = movieGenre;
+        this.duration = duration;
+    }
+
+    public String getShowName(){
+        return this.showName;
+    }
+
+    public String getShowGenre(){
+        return this.showGenre;
+    }
+
+    public int getDuration(){
+        return this.duration;
+    }
+
 }
