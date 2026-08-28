639d5f655a9af0c1ae1d9acf014970740c3fa33f
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-28 21:23:40 +0545

    set up the customer and the admin page

--- Movie Ticket Booking System/src/model/Admin.java
+++ Movie Ticket Booking System/src/model/Admin.java
@@ -1,4 +1,22 @@
 package model;
 
-public class Admin {
+public class Admin extends User{
+
+    @Override
+    public void showMenu(){
+        System.out.println("1. Add Movies");
+        System.out.println("2. Update Movies");
+        System.out.println("3. Remove Movie");
+        System.out.println("4. View Movie");
+        System.out.println("5. Add show");
+        System.out.println("6. Update Show");
+        System.out.println("7. Remove Show");
+        System.out.println("8. View all Shows");
+        System.out.println("View Registered Customers");
+    }
+
+    Admin(int userId, String userName, String userPassword,String userEmail){
+        super(userId, userName,userPassword,userEmail);
+    }
+
 }
--- Movie Ticket Booking System/src/model/Customer.java
+++ Movie Ticket Booking System/src/model/Customer.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Customer {
+public class Customer extends User{
+
+    @Override
+    public void showMenu(){
+        System.out.println("1.Watch Movie");
+        System.out.println("2. Watch Shows");
+        System.out.println("3. Shows Screen");
+        System.out.println("4. Show Seats");
+        System.out.println("5. Booking a Show");
+        System.out.println("6. Ticket Details");
+    }
+
+    Customer(int userId, String userName, String userPassword, String userEmail){
+        super(userId,userName,userPassword,userEmail);
+    }
+
 }
