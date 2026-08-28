cbe611f4ee05f36209cfbcba4fcc558f1731e6db
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-28 21:09:51 +0545

    added user properties and behaviour

--- Movie Ticket Booking System/src/model/User.java
+++ Movie Ticket Booking System/src/model/User.java
@@ -1,4 +1,35 @@
 package model;
 
-public class User {
+public abstract class User {
+
+    private int userId;
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+
+    User(int userId, String userName, String userPassword, String userEmail){
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+    }
+
+    public int getUserId(){
+        return this.userId;
+    }
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public String getUserPassword(){
+        return this.userPassword;
+    }
+
+    public String getUserEmail(){
+        return this.userEmail;
+    }
+
+    public abstract void showMenu();
+
 }
